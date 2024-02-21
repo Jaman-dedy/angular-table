78d9b1f36b07d33a02db389224d7bcb11203dae4
Jaman-dedy <jeandedieuam@gmail.com>
2024-02-22 00:19:04 +0100

    add fetch commerces service

--- src/app/services/commerce.service.ts
+++ src/app/services/commerce.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { ICommerce } from '../types';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommerceService {
 
+  httpClient = inject(HttpClient);
   constructor() { }
+
+  getCommerce() {
+    return this.httpClient.get<ICommerce[]>('https://random-data-api.com/api/v2/beers?size=100')
+  }
 }
