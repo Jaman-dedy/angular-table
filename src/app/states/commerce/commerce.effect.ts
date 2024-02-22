b31e570ad7493416bf407b4d1da7ae6e7a8599c6
Jaman-dedy <jeandedieuam@gmail.com>
2024-02-22 01:08:09 +0100

    add fetch commerces effects

---
+++ src/app/states/commerce/commerce.effect.ts
@@ -0,0 +1,28 @@
+import { Injectable, inject } from "@angular/core";
+import { CommerceService } from "../../services/commerce.service";
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import * as CommerceActions from './commerce.action'
+import { catchError, map, of, switchMap } from "rxjs";
+
+@Injectable()
+export class CommerceEffect {
+    private service = inject(CommerceService);
+    actions$ = inject(Actions);
+
+    loadCommerces$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(CommerceActions.fetchCommerce),
+            switchMap(() =>
+                this.service.getCommerce().pipe(
+                    map((res) => CommerceActions.fetchCommerceSuccess({ commerces: res })),
+                    catchError((error: { message: string }) =>
+                        of(
+                            CommerceActions.fetchCommerceFailure({
+                                errorMessage: 'Fail to load commerces'
+                            })
+                        )
+                    )
+                ))
+        )
+    )
+}
